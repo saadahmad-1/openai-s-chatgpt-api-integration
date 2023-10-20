7f70783847e7039a695520a45cb7a2038e35e7da
Saad Ahmad <145578594+saadahmad-1@users.noreply.github.com>
2023-10-20 13:05:13 +0500

    Update practice-activity.py

--- practice-activity.py
+++ practice-activity.py
@@ -2,7 +2,7 @@
 import os 
 import openai
 
-os.environ['API_KEY'] = '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'
+os.environ['API_KEY'] = '<your api key goes here>'
 openai.api_key = os.environ.get('API_KEY')
 
 messages = [
